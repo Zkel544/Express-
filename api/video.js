57aa950e091fd739646ec86ee62574d410035af1
Zkel544 <batangdagung2@gmail.com>
2025-07-11 19:00:38 +0700

    update dan delete

--- api/video.js
+++ api/video.js
@@ -27,4 +27,47 @@ router.post('/video', async (req, res) => {
     }
 });
 
+router.post('/video/update/:id', async (req, res) => {
+    const { id } = req.params;
+    const { title, description, url } = req.body;
+    if (!title || !description || !url) {
+        return res.status(400).json({ message: "Isi semua formulir" });
+    }
+    try {
+        const updatedVideo = await Video.findByIdAndUpdate(
+            id,
+            { title, description, url },
+            { new: true }
+        );
+
+        if (!updatedVideo) {
+            return res.status(404).json({ message: "Video tidak ditemukan" });
+        }
+
+        res.status(200).json({
+            message: "Video berhasil diperbarui",
+            video: updatedVideo
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Terjadi kesalahan saat memperbarui video" });
+    }
+});
+
+router.post('/video/delete/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const deletedVideo = await Video.findByIdAndDelete(id);
+
+        if (!deletedVideo) {
+            return res.status(404).json({ message: "Video tidak ditemukan" });
+        }
+
+        res.status(200).json({ message: "Video berhasil dihapus" });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Terjadi kesalahan saat menghapus video" });
+    }
+});
+
 module.exports = router;
