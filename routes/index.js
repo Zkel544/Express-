3c17fb3a3544cd749e0d48fd2c6117e82d845af7
Zkel544 <batangdagung2@gmail.com>
2025-07-27 20:44:38 +0700

    Sampah

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,9 @@ const authenticateToken = require('../middleware/auth');
 const router            = express.Router();
 
 router.use('/user', require('../api/user'));
-router.use('/kategori',require('../api/kategori'));
+router.use('/kategori',authenticateToken ,require('../api/kategori'));
 router.use('/video', authenticateToken , require('../api/video'));
 router.use('/tps', authenticateToken, require('../api/tps'));
-router.use('/sampah', require('../api/sampah'));
+router.use('/sampah', authenticateToken ,require('../api/sampah'));
 
 module.exports = router;
