01e2acf2f122103c790f7489c19167b1dae9100e
codewithwil <willyrandika0603@gmail.com>
2025-08-14 20:55:23 +0700

    minor

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.use('/user', require('../api/user'));
 router.use('/kategori',authenticateToken ,require('../api/kategori'));
 router.use('/video', authenticateToken , require('../api/video'));
 router.use('/tps', authenticateToken, require('../api/tps'));
-router.use('/sampah', authenticateToken ,require('../api/sampah'));
+router.use('/sampah', authenticateToken ,require('../api/sampah')); 
 router.use('/service', authenticateToken ,require('../api/service'));
 router.use('/layanan', authenticateToken ,require('../api/layanan'));
 
